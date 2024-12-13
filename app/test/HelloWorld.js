85ed5ac4acd5f452f45a96e2e54fba821d8dffae
cm-adc <christopher@ascensus.digital>
2024-12-13 15:12:20 -0800

    Patch (#73)
    * exclude unit tests from coverage
    
    * add coverage to Sonar
    
    * fix code smell
    
    * fix code smell
    
    * test build failure on Sonar quality gate failure
    
    * fix
    
    ---------
    
    Co-authored-by: Christopher Marsh <christopher@ascensusglobal.com>

---
+++ app/test/HelloWorld.js
@@ -0,0 +1,6 @@
+const HelloWorld = require('../src/js/HelloWorld.js');
+const hw = new HelloWorld();
+
+// test('renders expected output', () => {
+//   expect(hw.render()).toBe('Hello World!');
+// });
