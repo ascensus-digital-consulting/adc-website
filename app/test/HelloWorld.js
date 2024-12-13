b0fc469ae9092edab73b19ef7a92c889021f8aaf
Christopher Marsh <christopher@ascensusglobal.com>
2024-12-13 15:27:01 -0800

    fix

--- app/test/HelloWorld.js
+++
@@ -1,6 +0,0 @@
-const HelloWorld = require('../src/js/HelloWorld.js');
-const hw = new HelloWorld();
-
-// test('renders expected output', () => {
-//   expect(hw.render()).toBe('Hello World!');
-// });
