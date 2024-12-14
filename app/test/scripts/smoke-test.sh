0a8a116e2b6d7fc63a1878acfc5a93f610d2e8ac
cm-adc <christopher@ascensus.digital>
2024-12-13 16:52:00 -0800

    color fix (#83)
    Co-authored-by: Christopher Marsh <christopher@ascensusglobal.com>

--- app/test/scripts/smoke-test.sh
+++ app/test/scripts/smoke-test.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
 url=$1
+export TERM=xterm-256color
+
 echo "Initiating smoke test against $url"
 http_response=$(curl -o /dev/null --silent --write-out '%{http_code}\n' $url)
-echo "HTTP response code $url received"
+echo "HTTP response code $http_response received"
+
 if [ $http_response -eq 200 ]; then
     tput setaf 2
     echo "Smoke test PASSED!"
-    echo "Exit code 0"
     exit 0
 else
     tput setaf 1
     echo "Smoke test FAILED!"
+    echo "Expected HTTP status 200, received HTTP status code $http_code"
     echo "Exit code 1"
     exit 1
 fi
