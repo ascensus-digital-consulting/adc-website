67f028631c69879c740aa09a5627819012febd98
Christopher Marsh <christopher@ascensusglobal.com>
2024-12-15 13:07:16 -0800

    update smoke test

--- app/test/scripts/smoke-test.sh
+++ app/test/scripts/smoke-test.sh
@@ -2,16 +2,20 @@
 url=$1
 export TERM=xterm-256color
 
+
+tput setaf 3
 echo "Initiating smoke test against $url"
 http_response=$(curl -o /dev/null --silent --write-out '%{http_code}\n' $url)
-echo "HTTP response code $http_response received"
+echo "Smoke test against $url complete"
 
 if [ $http_response -eq 200 ]; then
     tput setaf 2
+    echo "HTTP response code $http_response received"
     echo "Smoke test PASSED!"
     exit 0
 else
     tput setaf 1
+    echo "HTTP response code $http_response received"
     echo "Smoke test FAILED!"
     echo "Expected HTTP status 200, received HTTP status code $http_code"
     echo "Exit code 1"
