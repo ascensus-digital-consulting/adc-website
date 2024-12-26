477a76d2572759d670582f7e5124da039362ded5
Christopher Marsh <christopher@ascensus.digital>
2024-12-25 22:44:19 -0800

    cdk exploration (#250)
    Co-authored-by: Christopher Marsh <christopher@ascensusglobal.com>

---
+++ infra/lib/infra-stack.js
@@ -0,0 +1,24 @@
+const cdk = require('aws-cdk-lib');
+const iam = require('aws-cdk-lib/aws-iam');
+const kms = require('aws-cdk-lib/aws-kms');
+const s3 = require('aws-cdk-lib/aws-s3');
+const { Construct } = require('constructs');
+
+class InfraStack extends cdk.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const s3Bucket = new s3.Bucket(this, 'exampleBucket', {
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      bucketName: `example-bucket-adc-${new Date().getTime()}`,
+      // encryptionKey: new kms.Key(this, 's3BucketKMSKey'),
+    });
+
+    s3Bucket.grantRead(new iam.AccountRootPrincipal());
+  }
+}
+
+module.exports = { InfraStack };
