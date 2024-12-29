c964ebe726080028638b0037610031db2df077d1
Christopher Marsh <christopher@ascensus.digital>
2024-12-29 11:07:39 -0800

    Infra (#275)
    * add cloudfront distribution cdk code
    
    * infra update
    
    * infra update
    
    * security fix
    
    * infra update
    
    * infra update
    
    * infra update
    
    * infra updates
    
    * infra update
    
    ---------
    
    Co-authored-by: Christopher Marsh <christopher@ascensusglobal.com>

--- infra/lib/infra-stack.js
+++ infra/lib/infra-stack.js
@@ -1,49 +1,82 @@
-// Import the AWS CDK libraries
 const cdk = require('aws-cdk-lib');
 const { Stack } = cdk;
 const s3 = require('aws-cdk-lib/aws-s3');
-const s3deploy = require('@aws-cdk/aws-s3-deployment');
+const s3deploy = require('aws-cdk-lib/aws-s3-deployment');
 const cloudfront = require('aws-cdk-lib/aws-cloudfront');
 const origins = require('aws-cdk-lib/aws-cloudfront-origins');
 const iam = require('aws-cdk-lib/aws-iam');
+const route53 = require('aws-cdk-lib/aws-route53');
+const targets = require('aws-cdk-lib/aws-route53-targets');
+const { Certificate } = require('aws-cdk-lib/aws-certificatemanager');
 
 class InfraStack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // Create the S3 bucket with all public access blocked
-    const bucket = new s3.Bucket(this, 'MyBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // Block all public access
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Change to RETAIN for production
-      autoDeleteObjects: true, // Deletes objects when the bucket is deleted (useful for development)
+    const host = this.node.tryGetContext('host');
+    const bucketName = 'adc-s3-web';
+    const distributionName = 'adc-cloudfront-web';
+    const deploymentName = 'adc-deployment-web';
+    const aliasRecordName = 'adc-alias-record';
+    const domains = this.#defineDomains(host);
+
+    const bucket = this.#createBucket(bucketName);
+    const distribution = this.#createDistribution(
+      distributionName,
+      domains,
+      bucket
+    );
+    this.#createDeployment(deploymentName, bucket);
+    this.#createAliasRecord(aliasRecordName, host, distribution);
+
+    // Output the S3 bucket name and CloudFront distribution domain name
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: bucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontDomainName', {
+      value: distribution.domainName,
+    });
+  }
+
+  #createBucket(bucketName) {
+    const bucket = new s3.Bucket(this, bucketName, {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
       enforceSSL: true,
     });
 
-    // Create an Origin Access Control (OAC) for CloudFront to securely access the S3 bucket
-    const oac = new cloudfront.CfnOriginAccessControl(this, 'MyOAC', {
-      originAccessControlConfig: {
-        name: 'MyOAC',
-        originAccessControlOriginType: 's3',
-        signingBehavior: 'always',
-        signingProtocol: 'sigv4',
-      },
-    });
+    return bucket;
+  }
 
-    // Create a CloudFront distribution
+  #createDistribution(distributionName, domains, bucket) {
+    const defaultBehavior = {
+      origin: origins.S3BucketOrigin.withOriginAccessControl(bucket),
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS, // Enforce HTTPS
+      allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD, // Allow only GET and HEAD methods
+      cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD, // Cache GET and HEAD methods
+      cachePolicy: new cloudfront.CachePolicy(this, 'ADCCachePolicyWeb', {
+        defaultTtl: cdk.Duration.minutes(30), // Set the default TTL to 30 minutes
+        minTtl: cdk.Duration.minutes(30), // Set the minimum TTL to 30 minutes
+        maxTtl: cdk.Duration.minutes(60), // Set the maximum TTL to 60 minutes
+      }),
+    };
+    const props = {
+      defaultBehavior: defaultBehavior,
+      domainNames: domains, // Add your domain names here
+      defaultRootObject: 'index.html', // Set the default root object to index.html
+      certificate: Certificate.fromCertificateArn(
+        this,
+        'ADCCertificateWeb',
+        'arn:aws:acm:us-east-1:030460844096:certificate/43b0f99a-a402-4ff1-916c-687c79bcef1d' // Replace with your certificate ARN
+      ),
+    };
     const distribution = new cloudfront.Distribution(
       this,
-      'MyCloudFrontDistribution',
-      {
-        defaultBehavior: {
-          origin: new origins.S3BucketOrigin(bucket, {
-            originAccessControlId: oac.attrId, // Attach the OAC to the S3 origin
-          }),
-          viewerProtocolPolicy:
-            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS, // Enforce HTTPS
-        },
-      }
+      distributionName,
+      props
     );
-
     // Grant the CloudFront OAC permissions to read from the S3 bucket
     bucket.addToResourcePolicy(
       new iam.PolicyStatement({
@@ -58,19 +91,55 @@ class InfraStack extends Stack {
       })
     );
 
-    const deployment = new s3deploy.BucketDeployment(this, 'DeployFiles', {
+    return distribution;
+  }
+
+  #createDeployment(deploymenName, bucket) {
+    const deployment = new s3deploy.BucketDeployment(this, deploymenName, {
       sources: [s3deploy.Source.asset('../web/src')], // 'folder' contains your empty files at the right locations
       destinationBucket: bucket,
     });
 
-    // Output the S3 bucket name and CloudFront distribution domain name
-    new cdk.CfnOutput(this, 'BucketName', {
-      value: bucket.bucketName,
-    });
+    return deployment;
+  }
 
-    new cdk.CfnOutput(this, 'CloudFrontDomainName', {
-      value: distribution.domainName,
-    });
+  #createAliasRecord(aliasRecordName, host, distribution) {
+    const hostedZoneId = 'Z02235921WTFRIR8NQIBR'; // get from props or SSM lookup (Z****)
+    const zoneName = 'ascensus.digital'; // get from props or SSM lookup (mydomain.com)
+    const zone = route53.HostedZone.fromHostedZoneAttributes(
+      this,
+      'ADCImportedZone',
+      {
+        hostedZoneId,
+        zoneName,
+      }
+    );
+
+    const props = {
+      target: route53.RecordTarget.fromAlias(
+        new targets.CloudFrontTarget(distribution)
+      ),
+      zone: zone,
+      deleteExisting: true,
+    };
+
+    if (host) {
+      props.recordName = host;
+    }
+
+    const aliasRecord = new route53.ARecord(this, aliasRecordName, props);
+
+    return aliasRecord;
+  }
+
+  #defineDomains(host) {
+    const domains = [];
+    if (!host) {
+      domains.push('www.ascensus.digital', 'ascensus.digital');
+    } else {
+      domains.push(`${host}.ascensus.digital`);
+    }
+    return domains;
   }
 }
 
