b2bb0039e52d2e2df7a257fb8dbc9aee7ff428bd
Christopher Marsh <christopher@ascensus.digital>
2025-01-02 15:55:10 -0800

    Infra5 (#292)
    * add dev deployment to build
    
    * infra update
    
    * fix
    
    * add rewrite function
    
    * add url rewrite for versioon file
    
    * fix infra bug
    
    * update metadata in version file
    
    * refactor version file to metadata file
    
    ---------
    
    Co-authored-by: Christopher Marsh <christopher@ascensusglobal.com>

--- infra/lib/versionRewriteFunction.js
+++ infra/lib/versionRewriteFunction.js
@@ -1,9 +1,9 @@
 function handler(event) {
   var request = event.request;
   var uri = request.uri;
-  var regex = /version\/?$/i;
+  var regex = /metadata\/?$/i;
 
-  uri = uri.replace(regex, 'version.json');
+  uri = uri.replace(regex, 'metadata.json');
   request.uri = uri;
 
   return request;
