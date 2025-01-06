a8fd64b9e04284bb7871cdae1254b189768f2adb
Christopher Marsh <christopher@ascensus.digital>
2025-01-06 12:26:29 -0800

    Infra8 (#340)
    * fix permissions
    
    * fix version resolution
    
    * infro refactor
    
    * fix sonar security issue
    
    * fix sonar security issue
    
    * ignore VS setting folder
    
    * fix syntax
    
    * fix sonar linting issue
    
    * fix sonar linting issue
    
    * add timezone to metadata
    
    * add authz to pre-prod environments
    
    * fix bug
    
    * fix cf function bug
    
    * fix filename
    
    * refactor viewer request handler to avoid duplicate code
    
    * fix file path
    
    * remove retired files
    
    * fix smoke tests
    
    * fiz file path
    
    * fix url bug
    
    * fix variable name
    
    * refactor viewer request event handler
    
    * refactor viewer request handler
    
    * refactor for mantainability
    
    * missed file from last commit
    
    * add test
    
    * add test
    
    * add tests for viewer request handler
    
    * add cdk testing
    
    * fix build bug
    
    * fix build bug
    
    * fix build bug
    
    * add cdk tests
    
    ---------
    
    Co-authored-by: Christopher Marsh <christopher@ascensusglobal.com>

--- infra/test/lib/ADCWebInfraStack.test.js
+++ infra/test/lib/ADCWebInfraStack.test.js
@@ -4,31 +4,51 @@ const sns = require('aws-cdk-lib/aws-sns');
 const { ADCWebInfraStack } = require('../../lib/ADCWebInfraStack');
 
 describe('ADCWebInfraStack', () => {
-  test('synthesizes the way we expect', () => {
-    const app = new cdk.App();
+  let template;
 
+  beforeAll(() => {
     const context = {
       resourceNames: {
-        aliasRecord: 'aliasRecordName',
-        bucket: 'bucketName',
-        cachePolicy: 'cachePolicyName',
-        deployment: 'deploymentName',
-        distribution: 'distributionName',
-        stack: 'stackName',
-        viewerRequestHandler: 'viewerRequestHandlerName',
+        aliasRecord: 'ADCWevTestAliasRecordName',
+        bucket: 'ADCWevTestBucketName',
+        cachePolicy: 'ADCWevTestCachePolicyName',
+        deployment: 'ADCWevTestDeploymentName',
+        distribution: 'ADCWevTestDistributionName',
+        stack: 'ADCWevTestStackName',
+        viewerRequestHandler: 'vADCWevTestViewerRequestHandlerName',
       },
-      domains: 'domains',
-      host: 'host',
-      hostedZoneId: 'hostedZoneId',
-      zoneName: 'zoneName',
+      domains: 'test1.domain.test, test2.domain.test',
+      host: 'test1',
+      hostedZoneId: 'AAAAA11111BBBBB22222',
+      zoneName: 'domain.test',
     };
-
-    // Create the StateMachineStack.
+    const app = new cdk.App();
     const stack = new ADCWebInfraStack(app, 'ADCWebInfraStack', {
       env: { context: context },
     });
+    template = Template.fromStack(stack);
+  });
 
-    // Prepare the stack for assertions.
-    const template = Template.fromStack(stack);
+  test('bucket is created', () => {
+    template.hasResourceProperties('AWS::S3::Bucket', {});
+    template.hasResourceProperties('AWS::S3::BucketPolicy', {});
+  });
+
+  test('distribution is created', () => {
+    template.hasResourceProperties('AWS::CloudFront::CachePolicy', {});
+    template.hasResourceProperties('AWS::CloudFront::Distribution', {});
+    template.hasResourceProperties('AWS::CloudFront::Function', {});
+    template.hasResourceProperties('AWS::CloudFront::OriginAccessControl', {});
+  });
+
+  test('deployment is created', () => {
+    template.hasResourceProperties('AWS::IAM::Policy', {});
+    template.hasResourceProperties('AWS::IAM::Role', {});
+    template.hasResourceProperties('AWS::Lambda::Function', {});
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {});
+  });
+
+  test('dns record is created', () => {
+    template.hasResourceProperties('AWS::Route53::RecordSet', {});
   });
 });
