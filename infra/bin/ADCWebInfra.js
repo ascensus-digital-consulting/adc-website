d67753cbd2dfc9eedbf862dc8bde5dc9b330802c
Christopher Marsh <christopher@ascensus.digital>
2025-01-11 11:28:08 -0800

    Add iam group (#350)
    * refactor for maintainability and testability
    
    * refactor permissions
    
    * refactor cdk entry point
    
    ---------
    
    Co-authored-by: Christopher Marsh <christopher@ascensusglobal.com>

--- infra/bin/ADCWebInfra.js
+++ infra/bin/ADCWebInfra.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
-
 const cdk = require('aws-cdk-lib');
 const { ADCWebInfraStack } = require('../lib/ADCWebInfraStack');
 
 /*** Setup ***/
 const app = new cdk.App();
 const props = ADCWebInfraStack.configureStackProps(app);
+const id = props.env.context.resourceNames.stack;
 
 /*** Create stack ***/
-const stack = new ADCWebInfraStack(
-  app,
-  props.env.context.resourceNames.stack,
-  props
-);
+const stack = new ADCWebInfraStack(app, id, props);
